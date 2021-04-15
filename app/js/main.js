1d6806d91b1dfe6044662faabddd0bcc99f86140
yanaT <redish_girl@mail.ru>
2021-04-15 23:38:05 +0500

    header, burger

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
-$(function () {
+// burger-menu
 
-})
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+if (iconMenu) {
+    iconMenu.addEventListener('click', function () {
+        document.body.classList.toggle('lock');
+        iconMenu.classList.toggle('active');
+        menuBody.classList.toggle('active');
+    })
+}
--- gulpfile.js
+++ gulpfile.js
@@ -53,8 +53,6 @@ function styles() {
 function scripts() {
     return src([
         './node_modules/jquery/dist/jquery.js',
-        './node_modules/slick-carousel/slick/slick.js',
-        './node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js',
         './app/js/main.js',
     ])
         .pipe(concat('main.min.js'))

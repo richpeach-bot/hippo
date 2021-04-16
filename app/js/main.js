b916b57283a0fc0cc2c5d59c4adf30ee31a33896
yanaT <redish_girl@mail.ru>
2021-04-16 14:05:56 +0500

    card

--- app/js/main.js
+++ app/js/main.js
@@ -9,3 +9,33 @@ if (iconMenu) {
         menuBody.classList.toggle('active');
     })
 }
+
+
+// sliders
+
+import SwiperCore, { Navigation, Pagination } from 'swiper/core';
+
+  // configure Swiper to use modules
+SwiperCore.use([Navigation, Pagination]);
+
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });  

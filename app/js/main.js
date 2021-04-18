9b5ccaf31b1a576c45fd78fc3ebb5ef9d0aa2d9b
yanaT <redish_girl@mail.ru>
2021-04-18 20:12:32 +0500

    gallery update

--- app/js/main.js
+++ app/js/main.js
@@ -10,48 +10,24 @@ if (iconMenu) {
   })
 };
 
-
-// sliders
-
-// const swiper = new Swiper('.swiper-container', {
-//   loop: true,
-//   slidesPerView: 2,
-//   spaceBetween: 30,
-//   // autoplay: {
-//   //   delay: 2500,
-//   //   disableOnInteraction: false,
-//   // },
-//   pagination: {
-//     el: '.swiper-pagination',
-//     clickable: true,
-//   },
-// });
-
+// slider
 
 (function () {
 
   'use strict';
-
-  // breakpoint where swiper will be destroyed
-  // and switches to a dual-column layout
   const breakpoint = window.matchMedia('(min-width:1023px)');
   let mySwiper;
 
   const breakpointChecker = function () {
-
-    // if larger viewport and multi-row layout needed
     if (breakpoint.matches === true) {
 
-      // clean up old instances and inline styles when available
       if (mySwiper !== undefined) mySwiper.destroy(true, true);
 
-      // or/and do nothing
       return;
 
-      // else if a small viewport and single column layout needed
+
     } else if (breakpoint.matches === false) {
 
-      // fire small viewport version of swiper
       return enableSwiper();
 
     }
@@ -62,38 +38,17 @@ if (iconMenu) {
 
     mySwiper = new Swiper('.swiper-container', {
       loop: true,
-      // slidesPerView: 3,
-      loop: true,
-      // spaceBetween: 20,
-
+      slidesPerView: 1,
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
-      },
-
-
-      breakpoints: {
-        // when window width is >= 320px
-        320: {
-          // centeredSlides: true,
-          slidesPerView: 1,
-        },
-        // when window width is >= 640px
-        // 1330: {
-        //   slidesPerView: 2,
-        //   spaceBetween: 20
-        // },
       }
     });
 
   };
 
-  // keep an eye on viewport size changes
   breakpoint.addListener(breakpointChecker);
 
-  // kickstart
   breakpointChecker();
-
-
 
 })();

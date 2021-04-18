16b2786f06c3b9f6517f34a92a5fa9c4d6f5bd04
yanaT <redish_girl@mail.ru>
2021-04-18 17:50:51 +0500

    prodgect structure

--- app/js/main.js
+++ app/js/main.js
@@ -3,39 +3,97 @@
 const iconMenu = document.querySelector('.menu__icon');
 const menuBody = document.querySelector('.menu__body');
 if (iconMenu) {
-    iconMenu.addEventListener('click', function () {
-        document.body.classList.toggle('lock');
-        iconMenu.classList.toggle('active');
-        menuBody.classList.toggle('active');
-    })
+  iconMenu.addEventListener('click', function () {
+    document.body.classList.toggle('lock');
+    iconMenu.classList.toggle('active');
+    menuBody.classList.toggle('active');
+  })
 };
 
 
 // sliders
 
-import SwiperCore, { Navigation, Pagination } from 'swiper/core';
+// const swiper = new Swiper('.swiper-container', {
+//   loop: true,
+//   slidesPerView: 2,
+//   spaceBetween: 30,
+//   // autoplay: {
+//   //   delay: 2500,
+//   //   disableOnInteraction: false,
+//   // },
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+// });
 
-  // configure Swiper to use modules
-SwiperCore.use([Navigation, Pagination]);
 
-const swiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });  
+(function () {
+
+  'use strict';
+
+  // breakpoint where swiper will be destroyed
+  // and switches to a dual-column layout
+  const breakpoint = window.matchMedia('(min-width:1919px)');
+  let mySwiper;
+
+  const breakpointChecker = function () {
+
+    // if larger viewport and multi-row layout needed
+    if (breakpoint.matches === true) {
+
+      // clean up old instances and inline styles when available
+      if (mySwiper !== undefined) mySwiper.destroy(true, true);
+
+      // or/and do nothing
+      return;
+
+      // else if a small viewport and single column layout needed
+    } else if (breakpoint.matches === false) {
+
+      // fire small viewport version of swiper
+      return enableSwiper();
+
+    }
+
+  };
+
+  const enableSwiper = function () {
+
+    mySwiper = new Swiper('.swiper-container', {
+      loop: true,
+      // slidesPerView: 3,
+      loop: true,
+      // spaceBetween: 20,
+
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+
+      breakpoints: {
+        // when window width is >= 320px
+        320: {
+          // centeredSlides: true,
+          slidesPerView: 1,
+        },
+        // when window width is >= 640px
+        // 1330: {
+        //   slidesPerView: 2,
+        //   spaceBetween: 20
+        // },
+      }
+    });
+
+  };
+
+  // keep an eye on viewport size changes
+  breakpoint.addListener(breakpointChecker);
+
+  // kickstart
+  breakpointChecker();
+
+
+
+})();
